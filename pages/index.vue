c8859a9ad0911c2b5a0a70774ca53b484fce0819
Jakub Voráček <jakub.voracek@gmail.com>
2022-03-31 18:10:50 +0400

    publish dogs

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <h1>Test whether it works</h1>
+  <div class="bg-gray-100 h-screen flex items-center justify-center">
+    <div class="rounded-xl overflow-hidden shadow-xl">
+      <img
+        :src="img.message"
+        alt=""
+        v-if="img.status === 'success'"
+        height="360"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
+
+  data() {
+    return {
+      img: "",
+    };
+  },
+
+  async fetch() {
+    this.img = await this.$axios.$get(
+      "https://dog.ceo/api/breeds/image/random"
+    );
+  },
 };
 </script>
