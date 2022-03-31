f1383c06f33f6f5999263a721ddd451c063df1f0
Jakub Voráček <jakub.voracek@gmail.com>
2022-03-31 19:13:13 +0400

    change api

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="bg-gray-100 h-screen flex items-center justify-center">
-    <div class="rounded-xl overflow-hidden shadow-xl">
-      <img
-        :src="img.message"
-        alt=""
-        v-if="img.status === 'success'"
-        height="360"
-      />
+    <div class="rounded-xl overflow-hidden shadow-xl bg-white px-8 py-6">
+      <h1 class="text-xl font-semibold text-gray-900">{{ res.activity }}!</h1>
     </div>
   </div>
 </template>
@@ -17,14 +12,12 @@ export default {
 
   data() {
     return {
-      img: "",
+      res: "",
     };
   },
 
   async fetch() {
-    this.img = await this.$axios.$get(
-      "https://dog.ceo/api/breeds/image/random"
-    );
+    this.res = await this.$axios.$get("https://www.boredapi.com/api/activity");
   },
 };
 </script>
